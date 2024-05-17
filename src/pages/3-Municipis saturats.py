06eb1b999eef1047f6bd1cca3f78c1233a097194
Miquel Piulats <miquel@softcatala.org>
2024-05-17 18:53:01 +0200

    update

--- src/pages/3-Municipis saturats.py
+++ src/pages/3-Municipis saturats.py
@@ -12,27 +12,49 @@ st.set_page_config(
 
 
 # Títol
-st.title("Saturació turśitica per municipis de Mallorca")
+st.title("Saturació turística per municipis de Mallorca")
 
 
 '''
-Per a calcula un índex de saturació turística per municipi, hem tingut en compte la relació entre la població resident i el nombre de places totals ofertades. Aquest índex ens permetrà identificar els municipis més saturats i, per tant, on la pressió turística és més elevada.
+S'ha calculat un un índex de saturació turística per municipi, hem tingut en compte la relació entre la població resident i el nombre de places totals ofertades. 
 
+Aquest índex ens permet identificar els municipis amb més intensitat de turisme, més pressió sobre els recursos locals i sobre la gestió municipal.
+
+La lectura de l'índex és equivalent al nombre de places turístiques per  habitant del municipi.
 '''
 
-# Càrrega habitatges turístics
+# Carrega les dades
 df_habitatges = pd.read_csv("data/Habitatges.csv")
-df_habitatges_municipi = df_habitatges.groupby('Municipi').size()
+df_allotjaments = pd.read_csv("data/Allotjaments.csv")
+
+# Agrupar por 'Municipi' i sumar les places turístiques
+df_habitatges_grp_places = df_habitatges.groupby('Municipi')['Places'].sum().reset_index()
+df_allotjaments_grp_places = df_allotjaments.groupby('Municipi')['Places'].sum().reset_index()
+
+
+df_places_total_municipis = pd.merge(df_habitatges_grp_places, df_allotjaments_grp_places, on='Municipi', suffixes=('_habitatges', '_allotjaments'))
+df_places_total_municipis['Places'] = df_places_total_municipis['Places_habitatges'] + df_places_total_municipis['Places_allotjaments']
+
+# Dades de població
+df_poblacio = pd.read_csv("data/Poblacio.csv")
+# Reduim columnes
+df_poblacio = df_poblacio[['Municipi', 'Total']]
+df_poblacio.rename(columns={'Total': 'Població'}, inplace=True)
+# Format del municipi a majuscules
+df_poblacio['Municipi'] = df_poblacio['Municipi'].str.upper()
+
+df_final = pd.merge(df_places_total_municipis, df_poblacio, on='Municipi', how='left')
+
+# Calculem índex de places turístiques per habitants del municipi.
+df_final['Índex saturació'] = (df_final['Places'] / df_final['Població']).round(2)
 
 # Càrrega geojson municipis
 df_geojson = gpd.read_file("data/mallorca.json")
 # Transformem camp per poder relacionar les dades
 df_geojson['Municipi'] = df_geojson['neighbourhood'].str.upper()
 
-df_habitatges_municipi = df_habitatges_municipi.reset_index()
-df_habitatges_municipi.columns = ['Municipi', 'Count']
-
-df_merged = pd.merge(df_geojson, df_habitatges_municipi, on='Municipi', how='inner')
+df_final = df_final.reset_index()
+df_merged = pd.merge(df_geojson, df_final, on='Municipi', how='inner')
 
 m = folium.Map(location=[39.55263, 2.9],
                zoom_start=10,
@@ -40,13 +62,13 @@ m = folium.Map(location=[39.55263, 2.9],
                scrollWheelZoom=False,
                dragging=False)
 
-colormap = LinearColormap(colors=['red', 'green'], vmin=df_merged['Count'].min(), vmax=df_merged['Count'].max())
+colormap = LinearColormap(colors=['red', 'green'], vmin=df_merged['Índex saturació'].min(), vmax=df_merged['Índex saturació'].max())
 
 folium.Choropleth(
     geo_data=df_merged,
     name="choropleth",
     data=df_merged,
-    columns= ["Municipi", 'Count'],
+    columns= ["Municipi", 'Índex saturació'],
     key_on="feature.properties.Municipi",
     fill_color="YlOrRd",
     fill_opacity=0.7,
@@ -54,8 +76,8 @@ folium.Choropleth(
     ).add_to(m)
 
 tooltip = folium.GeoJsonTooltip(
-    fields=["Municipi", 'Count'],
-    aliases=["", "Hab.Turístics:"],
+    fields=["Municipi", 'Places', 'Població', 'Índex saturació'],
+    aliases=["", "Places", 'Població', "Índex calculat:"],
     localize=True,
     sticky=False,
     labels=True
@@ -64,25 +86,8 @@ tooltip = folium.GeoJsonTooltip(
 folium.GeoJson(df_merged, name="Mapa de municipis de Mallorca", tooltip=tooltip).add_to(m)
 
 '''
-En general, com podem observar, els municipis amb més oferta d'allotjaments turístics són els situats a la costa i que disposen de platges. Destaca especialment el municipi de Pollença.
+La saturació turística està especialment present en municipis de la costa i afecta especialment als que tenen una població petita.
 '''
 
 
-folium_static(m, width=1000, height=1000)
-
-disable_dragging_js = """
-<script>
-document.addEventListener("DOMContentLoaded", function() {
-alert('')
-    var map = document.getElementsByClassName('folium-map')[0];
-    map.style.cursor = 'default';
-    map.style.pointerEvents = 'none';
-
-    map.addEventListener('click', function(event) {
-        event.preventDefault();
-    });
-});
-</script>
-"""
-
-st.write(disable_dragging_js, unsafe_allow_html=True)
+folium_static(m, width=850, height=850)
